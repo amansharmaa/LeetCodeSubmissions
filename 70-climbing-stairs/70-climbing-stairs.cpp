9852733f4186f645f4b67e1e3c2d6044c851699a
amansharmaa <53538891+amansharmaa@users.noreply.github.com>
2022-01-24 13:58:33 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (97.11%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,15 @@
-unordered_map<int , int> memo;
-unordered_map<int,int>::iterator it;
-int stair(int n)
-{
-	it = memo.find(n);
-	if (it != memo.end())
-	{
-		return memo[n];
-	}
-	if(n<2){
-		return 1;
-	}
-	memo[n] = stair(n-1) + stair(n-2);
-	return memo[n];
-}
 class Solution {
 public:
     int climbStairs(int n) {
-       return stair(n);
+       	int t1 , a1=1 , a2=2;
+	t1 = n;
+	for (int i = 2; i < t1 ; i++)
+	{
+		n = a1 + a2;
+		a1 = a2;
+		a2 = n;
+	}
+		return n;
+
     }
 };
