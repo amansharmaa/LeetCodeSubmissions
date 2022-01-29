1d7cf3194df79555ebe881529403df0d1834febd
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-01-29 16:05:55 +0530

    Time: 16 ms (89.20%), Space: 16.9 MB (84.93%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> count;
-        for(int i=0;i<nums.size();i++)
-        {
-            count[nums[i]]++;
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(count[nums[i]] == 1)
-            {
-                return nums[i];
-            }
-        }
-        return 0;
+    int ans = 0;
+    for(int i=0;i<nums.size();i++)
+    {
+        ans ^= nums[i];
+    }
+        return ans;
     }
 };
