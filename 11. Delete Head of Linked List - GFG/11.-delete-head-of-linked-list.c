04c59adb93a5b2eb7d07bc04548411c45fcef5ce
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-13 19:53:03 +0530

    Added solution - LeetHub

---
+++ 11. Delete Head of Linked List - GFG/11.-delete-head-of-linked-list.c
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+//struct Node *start;
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+struct Node * deleteHead(struct Node *head)
+{
+    head=head->next;
+    return head;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      start = deleteHead(start);
+      display(start);
+      printf("\n");
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+ void display(struct Node *head)
+{
+   while(head!=NULL)
+  {
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
+
+
+
+  // } Driver Code Ends
