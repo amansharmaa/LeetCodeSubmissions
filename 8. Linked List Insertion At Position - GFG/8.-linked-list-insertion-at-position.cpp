44bdfa88423ecb3f3b08810a6a38548ca4a03a5c
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-13 17:31:19 +0530

    Added solution - LeetHub

---
+++ 8. Linked List Insertion At Position - GFG/8.-linked-list-insertion-at-position.cpp
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+void displayList(Node *head)
+{
+    while(head)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+//Complete this function
+void insertAtPosition(Node *head, int pos, int data)
+{
+    
+    int count=1;
+    Node *ptr=head;
+    Node *inp = new Node(data);
+    while(count<pos && ptr!=NULL)
+    {
+        ptr=ptr->next;
+        count++;
+    }
+    if(ptr==NULL) return;
+    inp->next=ptr->next;
+    ptr->next=inp;
+    return;
+}
+
+// { Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        int pos; cin >> pos;
+        int d; cin >> d; 
+        insertAtPosition(head, pos, d);
+        displayList (head);
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
