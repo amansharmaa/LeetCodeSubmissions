744fb8715b5301312f2a5c926cd402571c284644
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-13 20:24:03 +0530

    Added solution - LeetHub

---
+++ 12. Linked List Delete at Position - GFG/12.-linked-list-delete-at-position.cpp
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+
+void displayList(Node *head)
+{
+    while(head)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+*/
+
+Node * deleteAtPosition(Node *head,int pos)
+{
+    Node *ptr=head;
+    if(pos==1)
+    {
+        head=head->next;
+        return head;
+    }
+    int i=1;
+    while(i<pos-1)
+    {
+        ptr=ptr->next;
+        i+=1;
+    }
+    ptr->next=ptr->next->next;
+    return head;
+}
+
+// { Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        int pos;
+	    cin>>pos;
+	    head=deleteAtPosition(head,pos);
+    	displayList(head);
+	    cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
