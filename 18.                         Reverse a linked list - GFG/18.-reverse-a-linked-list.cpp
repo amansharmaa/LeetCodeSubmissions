859c6b76e4b811ce9466f213ac360435abab47b7
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-13 21:44:58 +0530

    Added solution - LeetHub

---
+++ 18.                         Reverse a linked list - GFG/18.-reverse-a-linked-list.cpp
@@ -0,0 +1,102 @@
+// { Driver Code Starts
+//Initial Template for C++// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+ // } Driver Code Ends
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+     {
+        // code here
+        // return head of reversed list
+        Node *ptr1=NULL;
+        Node *ptr2=head;
+        Node *ptr3=head->next;
+        // ptr2->next = NULL;
+        while(ptr3!=NULL){
+            ptr2->next=ptr1;
+            ptr1=ptr2;
+            ptr2=ptr3;
+            ptr3=ptr3->next;
+        }
+        ptr2->next=ptr1;
+        return ptr2;
+    }
+    
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
