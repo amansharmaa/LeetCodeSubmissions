47680e39c8189b7bcfb73a9eea96782f4f5b0e77
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-14 14:11:33 +0530

    Added solution - LeetHub

---
+++ 2. Length of Circular Linked List - GFG/2.-length-of-circular-linked-list.cpp
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+
+int getLength(Node * head)
+{
+    int ans=1;
+    Node *ptr=head;
+    head=head->next;
+    while(head!=ptr)
+    {
+    ans += 1;
+    head=head->next;
+    }
+    return ans;
+}
+
+
+
+
+// { Driver Code Starts.
+
+
+void makeCircular(Node * head)
+{
+    Node * temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+	    
+	    
+	 
+
+	    makeCircular(head);
+	    
+	    
+	    cout<<getLength(head);
+ 
+	    cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
