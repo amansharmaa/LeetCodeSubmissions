12dc79805b26242df99fb90058669835ad50c3a2
Aman Sharma <53538891+amansharmaa@users.noreply.github.com>
2022-05-15 17:59:34 +0530

    Added solution - LeetHub

---
+++ 7. Delete Tail of Circular Linked List - GFG/7.-delete-tail-of-circular-linked-list.cpp
@@ -0,0 +1,133 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+void displayList(Node *head)
+{
+    Node *temp=head;
+    while(temp->next!=head)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<temp->data<<" ";
+}
+
+
+//Back-end complete function Template for C++
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+*/
+Node * deleteTail(Node * head)
+{
+    Node *ptr = head;
+    head=head->next;
+    Node *prv = ptr;
+    while(head->next!=ptr)
+    {
+        head=head->next;
+        prv=prv->next;
+    }
+    prv->next=ptr;
+    return ptr;
+}
+
+// { Driver Code Starts.
+
+
+void makeCircular(Node * head)
+{
+    Node * temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+  
+
+	    makeCircular(head);
+	    
+	    head=deleteTail(head);
+	    
+	    displayList(head);
+	    
+	    cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
